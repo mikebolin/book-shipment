648c16bebb24d9fe0b51352a85f1deff4ea2ed4f
mikebolin <mikebolin@gmail.com>
2019-10-12 02:48:21 -0400

    2 files modified

--- src/app/book-shipment-button/book-shipment-button.component.ts
+++ src/app/book-shipment-button/book-shipment-button.component.ts
@@ -14,8 +14,6 @@ export class OrderConfirm implements OnInit {
   }
 
 ngOnInit(){
-var s="!!=cvuupo!bqqdmbttdibohfs>##!dmbtt>#psefs#?=tqbo!dmbtt>#efgbvmu#?Cppl!Tijqnfou=0tqbo?=tqbo!dmbtt>#tvddftt#?Tijqnfou!Cpplfe!!!!=twh!wjfxCpy>#1!1!23!21#!ynmot>#iuuq;00xxx/x4/psh031110twh#!ynmot;ymjol>#iuuq;00xxx/x4/psh02:::0ymjol#?!!!!!!=qpmzmjof!qpjout>#2/6!7!5/6!:!21/6!2#?=0qpmzmjof?!!!!=0twh?=0tqbo?!!=ejw!dmbtt>#cpy#?=0ejw?!!=ejw!dmbtt>#usvdl#?!!!!=ejw!dmbtt>#cbdl#?=0ejw?!!!!=ejw!dmbtt>#gspou#?!!!!!!=ejw!dmbtt>#xjoepx#?=0ejw?!!!!=0ejw?!!!!=ejw!dmbtt>#mjhiu!upq#?=0ejw?!!!!=ejw!dmbtt>#mjhiu!cpuupn#?=0ejw?!!=0ejw?!!=ejw!dmbtt>#mjoft#?=0ejw?=0cvuupo?";
-let m=""; for (let i=0; i<s.length; i++) {	if(s.charCodeAt(i) == 28){	  m+= '&';} else if (s.charCodeAt(i) == 23) {	  m+= '!';} else {	  m+=String.fromCharCode(s.charCodeAt(i)-1);	}}document.write(m);
 }
 
 
